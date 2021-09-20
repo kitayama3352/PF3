4626b49dfbc7e46e420d13a698501ba0fb39d4b1
kitayama3352 <kitayama3352@gmail.com>
2021-09-20 04:26:39 +0000

    いいね機能追加

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0cdee174c0d5c03eb27f4d0a77752d3eb953c49a56cc13d036aefde41e2912c15622508b60be5c83c999c06aea199d142840539f65ac4c4b051a3b1e17992348'
